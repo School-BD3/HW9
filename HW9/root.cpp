cad061217825a52ef9ca8d1893dc588fb01aa357
Tin-sMods <minemodst1n.s@gmail.com>
2025-10-18 18:31:44 +0300

    Тема 9

--- HW9/root.cpp
+++ HW9/root.cpp
@@ -1,42 +1,77 @@
 #include <iostream>
-#include <windows.h>
-#include "root.h"
+#include <cmath>
 using namespace std;
 
-int main() {
-    int choice;
-    while (true) {
-        cout << "\nMenu:\n";
-        cout << "1 - Find the perimeter of a square given its area\n";
-        cout << "2 - Find the area of the ring\n";
-        cout << "3 - Perimeter of a right triangle\n";
-        cout << "4 - Analysis of a two-digit number\n";
-        cout << "0 - Esc\n";
-        cout << "Your choice: ";
-        cin >> choice;
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 
-        if (choice == 0) {
-            cout << "Exiting the program...\n";
-            break;
-        }
 
-        switch (choice) {
-        case 1:
-            A();
-            break;
-        case 2:
-            B();
-            break;
-        case 3:
-            C();
-            break;
-        case 4:
-            D();
-            break;
-        default:
-            cout << "The selection is incorrect, please try again.\n";
-        }
+void A() {
+    double S;
+    cout << "Enter the area of the square: ";
+    cin >> S;
+
+    if (S <= 0) {
+        cout << "The area must be greater than 0!\n";
+        return;
     }
 
-    return 0;
+    double a = sqrt(S);
+    double P = 4 * a;
+    cout << "Perimeter of a square: " << P << endl;
+}
+
+void B() {
+    double R, r;
+    cout << "Enter the outer radius R: ";
+    cin >> R;
+    cout << "Enter the inner radius r: ";
+    cin >> r;
+
+    if (R <= 0 || r <= 0 || r >= R) {
+        cout << "Error: radii must be positive, and R > r.\n";
+        return;
+    }
+
+    double S = M_PI * (R * R - r * r);
+    cout << "Ring area: " << S << endl;
+}
+
+void C() {
+    double a, b;
+    cout << "Enter the side a: ";
+    cin >> a;
+    cout << "Enter the side b: ";
+    cin >> b;
+
+    if (a <= 0 || b <= 0) {
+        cout << "The cathetus must be positive!\n";
+        return;
+    }
+
+    double c = sqrt(a * a + b * b);
+    double P = a + b + c;
+    cout << "Perimeter of a triangle: " << P << endl;
+}
+
+void D() {
+    int num;
+    cout << "Enter a two-digit number: ";
+    cin >> num;
+
+    if (num < 10 || num > 99) {
+        cout << "The number is not a two-digit number!\n";
+        return;
+    }
+
+    int tens = num / 10;
+    int ones = num % 10;
+    int sum = tens + ones;
+    int product = tens * ones;
+
+    cout << "Dozens: " << tens << endl;
+    cout << "Units: " << ones << endl;
+    cout << "Sum of digits: " << sum << endl;
+    cout << "Product of numbers: " << product << endl;
 }
